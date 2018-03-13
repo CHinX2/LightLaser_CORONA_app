246f68a23ab99a5c159ef0a5197d3fab866e688a
CHinX2 <iris870425@gmail.com>
2018-03-14 00:26:09 +0800

    game setting

---
+++ main.lua
@@ -0,0 +1,8 @@
+----------------------------------
+-- Producer : CHinX2 
+-- Finish : 2017 06 13
+----------------------------------
+
+local composer = require( "composer" )
+
+composer.gotoScene( "m_title", options )  --options¤U¤è
